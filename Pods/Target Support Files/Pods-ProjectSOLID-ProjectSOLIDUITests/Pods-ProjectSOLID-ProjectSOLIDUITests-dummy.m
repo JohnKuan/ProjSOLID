78747a0476824121639787b941e70229cde18a97
JohnKuan <founders2008@hotmail.com>
2020-11-10 12:46:47 +0800

    init comit

---
+++ Pods/Target Support Files/Pods-ProjectSOLID-ProjectSOLIDUITests/Pods-ProjectSOLID-ProjectSOLIDUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectSOLID_ProjectSOLIDUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectSOLID_ProjectSOLIDUITests
+@end
---
+++ Pods/Target Support Files/Pods-ProjectSOLID/Pods-ProjectSOLID-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectSOLID : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectSOLID
+@end
